2da9585f702e9ac5f39f1989c5d59032ee0dea51
wanghong <htimeis007money>
2020-10-28 18:21:10 +0800

    business_key

--- src/main/java/com/hong/activiti/basic/QueryProcessList.java
+++ src/main/java/com/hong/activiti/basic/QueryProcessList.java
@@ -2,7 +2,9 @@ package com.hong.activiti.basic;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 
 import java.util.List;
@@ -18,12 +20,21 @@ public class QueryProcessList {
     public static void main(String[] args) {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         TaskService taskService = processEngine.getTaskService();
-        List<Task> taskList = taskService.createTaskQuery().processDefinitionKey("myProcess").taskAssignee("zhangsan").list();
+        List<Task> taskList = taskService.createTaskQuery().processDefinitionKey("myProcess").taskAssignee("lisi").list();
         for (Task task:taskList){
             System.out.println("流程实例ID:" + task.getProcessDefinitionId());
             System.out.println("任务ID:" + task.getId());
             System.out.println("任务负责人:" + task.getAssignee());
             System.out.println("任务名称:" + task.getName());
         }
+
+        System.out.println("=========================");
+        // 获取 businessKey
+        Task task = taskService.createTaskQuery().processDefinitionKey("myProcess").taskAssignee("lisi").singleResult();
+        //taskService.complete(task.getId());
+        String processInstanceId = task.getProcessInstanceId();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processDefinitionId(processInstanceId).singleResult();
+        System.out.println("businessKey = " + processInstance.getBusinessKey());
     }
 }
